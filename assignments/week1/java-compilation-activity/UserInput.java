b00f23f3304dc1681c701e7bfab1a355aa7719e2
Robert (Trey) Ratcliff <robert089@revature.net>
2022-06-17 18:00:46 -0400

    Populated Item class with fields and getters,setters,toString,equals, and hashCode.

--- assignments/week1/java-compilation-activity/UserInput.java
+++ assignments/week1/java-compilation-activity/UserInput.java
@@ -1,15 +1,28 @@
 
 public class UserInput{
 
+    /*
+        main takes user input from args[] and prints either:
+            a random double
+                or
+            a String based on an integer provided by the user.
+
+        @params String[] args
+     */
     public static void main(String[] args){
 
+        //In case of empty array
         if(args.length == 0){
             System.out.println("No argument giving");
         }
+        //Prints random double if only one input
         else if (args.length == 1) {
             double rand = Math.random();
             System.out.println(rand);
         }
+        /*Prints 2nd item in args based on first element.
+          ASSUMES first element is an integer and second element is a word.
+        */
         else{
             String s = args[0];
             int i = Integer.parseInt(s);
--- projects/Project 0/Project0/src/main/java/com/revature/models/Item.java
+++ projects/Project 0/Project0/src/main/java/com/revature/models/Item.java
@@ -2,9 +2,81 @@ package com.revature.models;
 
 import com.revature.daos.ItemDAOS;
 
+import java.util.Objects;
+
 public class Item implements ItemDAOS {
+    private int id;
+    private String name;
+    private String description;
+    private int price; //Will decide later on what data type.
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", available=" + available +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    private boolean available;
     @Override
     public void changePrice(Item i) {
-        
+
     }
 }
