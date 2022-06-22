4557fab0ddc5fe955c6790a84c4de0d111340198
Robert (Trey) Ratcliff <robert089@revature.net>
2022-06-22 13:05:00 -0400

    added test switch case and added method to verify returning user with postgres database.

--- projects/Project 0/Project0/src/main/java/com/revature/Driver.java
+++ projects/Project 0/Project0/src/main/java/com/revature/Driver.java
@@ -22,7 +22,8 @@ public class Driver {
         System.out.println("Welcome to the Store!");
         System.out.println("Select an option \n" +
                 "-1: Register\n" +
-                "-2: Login");
+                "-2: Login\n" +
+                "-3: Test");
         String choice = sc.nextLine();
 
         switch(choice){
@@ -32,8 +33,13 @@ public class Driver {
                 break;
             case "2":
                 //login user
-                loginMenu();
+                loginMenuPostgres();
                 //System.out.println("Working on logging");
+                break;
+            case "3":
+                int id = 11;
+                User u = us.getUserById(id);
+                System.out.println(u);
                 break;
             default:
                 System.out.println("Sorry, I did not recognize that option");
@@ -66,5 +72,18 @@ public class Driver {
         }
         System.out.println("Sorry. We could not find a user with those credentials.");
 
+    }
+
+    public static void loginMenuPostgres(){
+        User unverifiedUser = new User();
+        System.out.println("Login:");
+        System.out.println("Please enter your username:");
+        unverifiedUser.setUsername(sc.nextLine());
+        System.out.println("Please enter your password:");
+        unverifiedUser.setPassword(sc.nextLine());
+        if(us.getUser(unverifiedUser) != null){
+            System.out.println("Welcome Back!");
+        }
+        System.out.println("Sorry. We could not find a user with those credentials.");
     }
 }
