f15add1a4f0916e0450164b082ad7b655b50d753
Robert (Trey) Ratcliff <robert089@revature.net>
2022-06-22 12:34:31 -0400

    Added functionality to methods. Note: Intellij says at line 102 u.setUserId(rs.getInt("id")); will produce a NullPointerException

--- projects/Project 0/Project0/src/main/java/com/revature/daos/UserPostgreSQL.java
+++ projects/Project 0/Project0/src/main/java/com/revature/daos/UserPostgreSQL.java
@@ -1,6 +1,9 @@
 package com.revature.daos;
 
 import com.revature.models.User;
+import com.revature.util.ConnectionUtil;
+
+import java.io.IOException;
 import java.sql.*; //change to specifics later.
 
 public class UserPostgreSQL implements UserDAOS{
@@ -8,11 +11,47 @@ public class UserPostgreSQL implements UserDAOS{
     @Override
     public void insertUser(User u) {
 
+        String sql = "insert into users (username, password) values (?,?) returning id;";
+        try(Connection c = ConnectionUtil.getConnectionFromFile()){
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setString(1, u.getUsername());
+
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                u.insertUser(u);
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            System.out.println("File with credentials not found.");
+        }
     }
 
     @Override
     public User getById(int id) {
-        return null;
+        String sql = "select * from users where id = ?;";
+        User user = null;
+
+        try(Connection c = ConnectionUtil.getConnectionFromFile()){
+            PreparedStatement ps = c.prepareStatement(sql);
+
+            ps.setInt(1, id); // this refers to the 1st ? in the sql String
+
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()) {
+                user = new User();
+                user.setUserId(rs.getInt("id"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }catch(IOException e){
+            System.out.println("File with credentials not found.");
+        }
+        return user;
     }
 
     @Override
@@ -22,11 +61,55 @@ public class UserPostgreSQL implements UserDAOS{
 
     @Override
     public User getByUsername(String username) {
-        return null;
+        String sql = "select * from users where username  = ?;";
+        User u = null;
+
+        try (Connection c = ConnectionUtil.getConnectionFromFile()){
+            PreparedStatement ps = c.prepareStatement(sql);
+
+            ps.setString(1, username); // this refers to the 1st "?" in the sql string, allows to inject data
+
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()) {
+                u = new User();
+                u.setUserId(rs.getInt("id"));
+                u.setUsername(rs.getString("username"));
+                u.setPassword(rs.getString("password"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }catch(IOException e){
+            System.out.println("File with credentials not found.");
+        }
+
+        return u;
     }
 
     @Override
-    public User getByLogin(String username, String passwd) {
-        return null;
+    public User getByLogin(String username, String passwd) throws NullPointerException {
+        String sql = "select * from users where (username,password) values (?,?)";
+        User u = null;
+
+        try(Connection c = ConnectionUtil.getConnectionFromFile()){
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, passwd);
+
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                u.setUserId(rs.getInt("id"));
+                u.setUsername(rs.getString("username"));
+                u.setPassword(rs.getString("password"));
+                return u;
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }catch(IOException e){
+            System.out.println("File with credentials not found.");
+        }
+
+        return u;
     }
 }
