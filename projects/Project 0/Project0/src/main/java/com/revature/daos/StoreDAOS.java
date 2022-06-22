a6a7ad6a7e87f70c84ddb233f09fed766926e8cb
Robert (Trey) Ratcliff <robert089@revature.net>
2022-06-22 17:39:13 -0400

    added more implemented methods

--- projects/Project 0/Project0/src/main/java/com/revature/daos/StoreDAOS.java
+++ projects/Project 0/Project0/src/main/java/com/revature/daos/StoreDAOS.java
@@ -1,11 +1,15 @@
 package com.revature.daos;
 
 import com.revature.models.Item;
+import java.util.List;
 
 public interface StoreDAOS {
 
     Item getItems(Item i);
     void addItem(Item i);
-    Item removeItem();
+    Item removeItem(Item i);
+    Item getItemById(int id);
+    Item getItemByName(String itemName);
+    List<Item> getItemList();
 
 }
