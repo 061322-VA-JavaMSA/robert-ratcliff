22402532f0de445bfcc7651d81cedd65d15ee168
Robert (Trey) Ratcliff <robert089@revature.net>
2022-06-22 17:37:32 -0400

    Class to handle postgresql to retrieve from item table.

---
+++ projects/Project 0/Project0/src/main/java/com/revature/daos/ItemPostgresSQL.java
@@ -0,0 +1,110 @@
+package com.revature.daos;
+
+import com.revature.models.Item;
+import com.revature.models.User;
+import com.revature.util.ConnectionUtil;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ItemPostgresSQL implements ItemDAOS{
+    @Override
+    public void changePrice(Item i) {
+
+    }
+
+    @Override
+    public void insertItem(Item i) {
+        if(getItem(i) != null){
+            System.out.println("Can't insert item, as this item already exists in the database.");
+        }else{
+            String sql = "insert into item (item_name, price) values (?,?) returning id;";
+            try (Connection c = ConnectionUtil.getConnectionFromFile()) {
+                PreparedStatement ps = c.prepareStatement(sql);
+                ps.setString(1, i.getName());
+                ps.setFloat(2, i.getPrice());
+
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    i.insertItem(i);
+                    System.out.println("Item: " + i.getName() + " has been created.");
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                System.out.println("File with credentials not found.");
+            }
+        }
+
+    }
+
+    @Override
+    public Item getById(int id) {
+        String sql = "select * from item where id = ?;";
+        Item i = null;
+
+        try(Connection c = ConnectionUtil.getConnectionFromFile()){
+            PreparedStatement ps = c.prepareStatement(sql);
+
+            ps.setInt(1, id); // this refers to the 1st ? in the sql String
+
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()) {
+                i = new Item();
+                i.setId(rs.getInt("id"));
+                i.setName(rs.getString("item_name"));
+                i.setPrice(rs.getFloat("price"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }catch(IOException e){
+            System.out.println("File with credentials not found.");
+        }
+        return i;
+    }
+
+    @Override
+    public Item getItem(Item i) {
+        if(i.getId() != 0){
+            return getById(i.getId());
+        }else if(i.getName() != null){
+            return getByItemName(i.getName());
+        }
+        else {
+            return null;
+        }
+    }
+
+    @Override
+    public Item getByItemName(String itemName) {
+        String sql = "select * from item where item_name  = ?;";
+        Item i = null;
+
+        try (Connection c = ConnectionUtil.getConnectionFromFile()){
+            PreparedStatement ps = c.prepareStatement(sql);
+
+            ps.setString(1, itemName);
+
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()) {
+                i = new Item();
+                i.setId(rs.getInt("id"));
+                i.setName(rs.getString("item_name"));
+                i.setPrice(rs.getFloat("price"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }catch(IOException e){
+            System.out.println("File with credentials not found.");
+        }
+
+        return i;
+    }
+}
