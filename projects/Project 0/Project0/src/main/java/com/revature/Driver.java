d006e5ba355e4f7d4ecd5f1299a5cf41e47330f4
Robert (Trey) Ratcliff <robert089@revature.net>
2022-06-23 17:34:18 -0400

    Added two methods for home menu and start menu. Also added ItemService instance.

--- projects/Project 0/Project0/src/main/java/com/revature/Driver.java
+++ projects/Project 0/Project0/src/main/java/com/revature/Driver.java
@@ -1,9 +1,12 @@
 package com.revature;
 
+import com.revature.models.Item;
 import com.revature.models.User;
 import com.revature.services.AuthService;
+import com.revature.services.ItemService;
 import com.revature.services.UserService;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Driver {
@@ -11,66 +14,86 @@ public class Driver {
     static Scanner sc;
     static  AuthService as;
     static UserService us;
+    static User u;
+    static ItemService itemService;
     //static StoreItems si; //will be used to test users being able to see items
     public static void main(String[] args){
 
         sc = new Scanner(System.in);
         as = new AuthService();
         us = new UserService();
+        u = new User();
+        itemService = new ItemService();
+        startMenu();
+    }
 
+    public static void startMenu(){
         System.out.println("Welcome to the Store!");
         System.out.println("Select an option \n" +
                 "-1: Register\n" +
                 "-2: Login\n" +
-                "-3: Test");
+                "-3: Employee");
         String choice = sc.nextLine();
 
         switch(choice){
             case "1":
                 //register user
                 registerMenu();
+                homeMenu(); //add logic to make sure user in registered and logged in
                 break;
             case "2":
                 //login user
                 loginMenuPostgres();
-                //System.out.println("Working on logging");
+                homeMenu(); //same a case "1"
                 break;
             case "3":
                 int id = 11;
                 User u = us.getUserById(id);
-                System.out.println(u);
+                boolean employee = u.isEmployee();
+                System.out.println("Is user " +u.getUsername()+ " an employee? " +employee);
                 break;
             default:
-                System.out.println("Sorry, I did not recognize that option");
+                System.out.println("Sorry, I did not recognize that option\n" +
+                        "Try again? (y/n)");
+                String resp = sc.nextLine();
+
+                if(resp.equalsIgnoreCase("y")){
+                    startMenu();
+                }
+                if(resp.equalsIgnoreCase("n")){
+                    sc.close();
+                    System.exit(0);
+                }
         }
     }
 
-    //Keeping code for now as logic, but will reimplement
+    public static void homeMenu(){
+        System.out.println("What do you want to do?\n" +
+                "1: View Store");
+        String choice = sc.nextLine();
+        switch(choice){
+            case "1":
+                List<Item> itemList = itemService.getAllItems();
+                System.out.println("Here are all the items:");
+                for (Item i: itemList) {
+                    System.out.print(i + "\n");
+                }
+                break;
+            default:
+                System.out.println("Sorry, didn't understand that.");
+        }
+    }
+
     public static void registerMenu(){
-        String username = null;
-        String passwd = null;
-        User newUser = new User(); //TODO cleanup this code to better handle new user.
+        User newUser = new User();
         System.out.println("Register:");
         System.out.println("Please enter your username:");
         newUser.setUsername(sc.nextLine());
         System.out.println("Please enter your password:");
         newUser.setPassword(sc.nextLine());
-        User createdUser = us.createUser(newUser);
+        u = us.createUser(newUser);
     }
 
-    public static void loginMenu(){
-        User unverifiedUser = new User();
-        System.out.println("Login:");
-        System.out.println("Please enter your username:");
-        unverifiedUser.setUsername(sc.nextLine());
-        System.out.println("Please enter your password:");
-        unverifiedUser.setPassword(sc.nextLine());
-        if(us.getUser(unverifiedUser) != null){
-            System.out.println("Welcome Back!");
-        }
-        System.out.println("Sorry. We could not find a user with those credentials.");
-
-    }
 
     public static void loginMenuPostgres(){
         User unverifiedUser = new User();
@@ -80,6 +103,7 @@ public class Driver {
         System.out.println("Please enter your password:");
         unverifiedUser.setPassword(sc.nextLine());
         if(us.getUser(unverifiedUser) != null){
+            u = unverifiedUser;
             System.out.println("Welcome Back, " +unverifiedUser.getUsername()+"!");
         }else {
             System.out.println("Sorry. We could not find a user with those credentials.\n" +
