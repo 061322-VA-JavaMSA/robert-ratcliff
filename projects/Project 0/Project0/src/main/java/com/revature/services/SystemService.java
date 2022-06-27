9594c4d8092cb77aa4073881cb9fc2fdd9ad79d1
Robert (Trey) Ratcliff <robert089@revature.net>
2022-06-27 17:25:46 -0400

    Functionality for services deemed as system functions added.

--- projects/Project 0/Project0/src/main/java/com/revature/services/SystemService.java
+++ projects/Project 0/Project0/src/main/java/com/revature/services/SystemService.java
@@ -1,7 +1,19 @@
 package com.revature.services;
 
+import com.revature.daos.SystemDAOS;
+import com.revature.daos.SystemPostgreSQL;
+import com.revature.models.Item;
 import com.revature.models.User;
 
-public class SystemService {
+import java.util.List;
 
+public class SystemService {
+    private SystemDAOS  sd = new SystemPostgreSQL();
+
+    public List getTotalPayments(){return sd.getAllPayments();}
+    public List getAllOffers(){return sd.getAllOffers();}
+    public float getItemOffer(User u, Item i){return sd.getItemOffer(u,i);}
+    public void updateOwnership(User u, Item i, float amount){sd.updateOwnership(u,i,amount);}
+    public float getWeeklyPayment(User u){return sd.calculateWeeklyPayment(u);}
+    public float getItemPayment(User u, Item i){return sd.calculateItemPayment(u, i);}
 }
