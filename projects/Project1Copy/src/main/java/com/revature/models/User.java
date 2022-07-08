af6feea17b9a0f34694ada2f5445d1440d109e64
Robert (Trey) Ratcliff <robert089@revature.net>
2022-07-08 17:29:14 -0400

    added getters and setters for role, firstName, lastName, and email

--- projects/Project1Copy/src/main/java/com/revature/models/User.java
+++ projects/Project1Copy/src/main/java/com/revature/models/User.java
@@ -39,7 +39,37 @@ public class User {
     public void setRole(Role role) {
         this.role = role;
     }*/
+    public String getRole() {
+        return role;
+    }
 
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
     @Override
     public String toString() {
         return "User{" +
