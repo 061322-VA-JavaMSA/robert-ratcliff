7f760f9b443f9212035c4cab926199e4ccb5dfd6
Robert (Trey) Ratcliff <robert089@revature.net>
2022-07-11 17:59:24 -0400

    added a dynamic table that will put all the user's submitted reimbursements, or if the user is an admin, will display all the reimbursements.

--- projects/Project1Copy/src/main/webapp/scripts/reimburse.js
+++ projects/Project1Copy/src/main/webapp/scripts/reimburse.js
@@ -1,3 +1,61 @@
 if(!principal){
     window.location.href="./index.html";
 }
+
+if(principal.userRole == 'ADMIN'){
+    getReimbursements();
+}
+
+//will only grab the user's submitted reimbursements.
+if(principal.userRole !== 'ADMIN'){
+    console.log(principal);
+    getUserReimbursements();
+}
+
+async function getUserReimbursements(){
+    //grabs this user's id, so we can view their submitted reimbursements.
+
+    let response = await fetch(`${apiUrl}/reimburse/${principal.id}`,{
+        credentials: 'include'
+    });
+    if(response.status == 200){
+        let data = await response.json();
+
+        populateTable(data);
+    }
+}
+
+async function getReimbursements(){
+    let response = await fetch(`${apiUrl}/reimburse`,{
+        credentials: 'include'
+    });
+    if(response.status == 200){
+        let data = await response.json();
+
+        populateTable(data);
+    }
+}
+
+function populateTable(data){
+    let tableBody = document.getElementById('reimburse-tbody');
+
+    data.forEach(reimburse => {
+        let tr = document.createElement('tr');
+        let tdId = document.createElement('td');
+        let tdDescription = document.createElement('td');
+        let tdDueDate = document.createElement('td');
+        let tdStatus = document.createElement('td');
+
+        tdId.innerHTML = reimburse.id;
+        tdDescription.innerHTML = reimburse.description;
+        tdDueDate.innerHTML = reimburse.dueDate;
+        tdStatus.innerHTML = reimburse.status;
+
+        tr.append(tdId);
+        tr.append(tdDescription);
+        tr.append(tdDueDate);
+        tr.append(tdStatus);
+
+        tableBody.append(tr);
+    });
+}
