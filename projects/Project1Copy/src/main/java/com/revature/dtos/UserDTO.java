fba2f050ae951edd228738ab31d07859e881d30e
Robert (Trey) Ratcliff <robert089@revature.net>
2022-07-14 23:32:52 -0400

    added more fields to UserDTO to show user's first and last name and email.

--- projects/Project1Copy/src/main/java/com/revature/dtos/UserDTO.java
+++ projects/Project1Copy/src/main/java/com/revature/dtos/UserDTO.java
@@ -12,6 +12,9 @@ public class UserDTO {
 
     private int id;
     private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
     private Role userRole;
 
     public UserDTO() {
@@ -22,6 +25,9 @@ public class UserDTO {
     public UserDTO(User u) {
         id = u.getUserId();
         username = u.getUsername();
+        firstName = u.getFirstName();
+        lastName = u.getLastName();
+        email = u.getEmail();
         userRole = u.getRole();
     }
 
@@ -37,6 +43,31 @@ public class UserDTO {
     public void setUsername(String username) {
         this.username = username;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public Role getUserRole() {
         return userRole;
     }
@@ -46,24 +77,28 @@ public class UserDTO {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserDTO userDTO = (UserDTO) o;
-        return id == userDTO.id && username.equals(userDTO.username) && userRole == userDTO.userRole;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, username);
-    }
-
-    @Override
     public String toString() {
         return "UserDTO{" +
                 "id=" + id +
                 ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
                 ", userRole=" + userRole +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDTO userDTO = (UserDTO) o;
+        return id == userDTO.id && username.equals(userDTO.username) && Objects.equals(firstName, userDTO.firstName) && Objects.equals(lastName, userDTO.lastName) && Objects.equals(email, userDTO.email) && userRole == userDTO.userRole;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, firstName, lastName, email, userRole);
+    }
+
 }
